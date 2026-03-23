deea8d051eb81a8c5125c588a117ef23c1427edb
Никита <nikitaizboldin@MBP16s-MacBook-Pro.local>
2026-03-23 21:23:53 +0400

    Initial: WU Calculator PWA with voice input

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'wu-calc-v2';
+const URLS = [
+  './',
+  './index.html',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const clone = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return resp;
+    }))
+  );
+});
